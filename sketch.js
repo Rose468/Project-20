10f96feff98001e25ec00d09a4d3a123ed4f97b8
Rose468 <85027884+Rose468@users.noreply.github.com>
2021-06-14 21:48:50 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,51 @@
+var wall, thickness;
+var bullet,speed,weight;
+thickness=random(22,83)
+
+
+
+function setup() {
+  createCanvas(1600,400);
+  bullet = createSprite(50,200,90,50);
+  bullet.shapeColor= "yellow";
+  
+  wall =createSprite(1200, 200, thickness, height/2);
+  wall.shapeColor =color(80,80,80);
+  speed=random(223,321);
+  weight=random(30,52)
+  
+}
+
+function draw() {
+  background(0);  
+  
+ if(hasCollided(bullet,wall)){
+   bullet.velocityX=0;
+   
+   var damage=0.5*weight*speed*speed*speed/(thickness*thickness*thickness)
+  if(damage>10){
+    wall.shapeColor=color(255,0,0);
+  }
+if(damage<10){
+  wall.shapeColor=color(0,255,0);
+
+}
+else{
+  wall.shapeColor =color(80,80,80);
+}
+ }
+ 
+ bullet.collide(wall);
+  drawSprites();
+}
+
+function hasCollided(lbullet,lwall){
+bulletRightEdge=lbullet.x +lbullet.width;
+wallLeftEdge=lwall.x;
+if(bulletRightEdge>=WallLeftEdge){
+  return true;
+}
+else{
+  return false;
+}
+}
